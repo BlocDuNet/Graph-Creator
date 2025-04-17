f0c312a60fdd7ff6088a70ddeac808f341a713ec
BlocDuNet <contact@leblocdu.net>
2025-04-17 02:37:09 +0200

    Remove legacy

--- js/script.js
+++
@@ -1,9 +0,0 @@
-/**
- * Script d'initialisation - maintenu pour la compatibilité
- * Délègue la logique principale au système modulaire
- */
-
-// Le code principal de ce fichier a été déplacé vers la structure modulaire
-// Ce fichier est maintenu pour la compatibilité avec le HTML existant
-
-console.log('Initialisation du script legacy...');
